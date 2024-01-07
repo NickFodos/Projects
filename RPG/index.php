02af24db9138537d28d427b9266a56860e2a0b80
leon <alchemistflame885@gmail.com>
2024-01-07 01:46:27 -0500

    tgtr

---
+++ RPG/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>BEM VINDO AO RPG!!</h1>
+            <Label>JOGADOR AUSENTE</Label>
+        </nav>
+    </header>
+    <main>
+    <div id="informacoes_do_jogador">
+        <Label>NOME...</Label>
+        <p>Historia base</p>
+        <ul>
+            <li>vida</li>
+            <li>destreza</li>
+            <li>Força</li>
+            <li>Constituição</li>
+            <li>Inteligencia</li>
+            <li>Carisma</li>
+        </ul>
+
+    </div>
+    <div id="respiracao_do_jogador">
+       <table>
+       <thead>
+        <tr>
+            <th>NOME DA RESPIRAÇÃO</th>
+        </tr>
+       </thead>
+        <tbody>
+        <tr>
+            <th>NOME DA FORMA</th>
+            <td>DSDCIDDFIUEBFY</td>
+        </tr>
+        </tbody>
+       </table>
+    </div>
+    </main>
+</body>
+</html>
